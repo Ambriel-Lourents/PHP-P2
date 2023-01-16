24731ef1c34c028446a5efeb580b254e9caa68ce
Ambriel-Lourents <112871854+Ambriel-Lourents@users.noreply.github.com>
2023-01-16 14:56:59 +0100

    small fixes

--- opdracht_5/index.php
+++ opdracht_5/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>opdracht 5</title>
+    <title>Document</title>
 </head>
 <body>
     <form method="POST" action="">
@@ -28,7 +28,7 @@
             <option value="Frankrijk">Frankrijk</option>
             <option value="Spanje">Spanje</option>
         </select><br>
-        <label for="email">E-mailadres:</label><br>
+        <label for="email">Email:</label><br>
         <input type="email" id="email" name="email"><br>
         <label for="wachtwoord">Wachtwoord:</label><br>
         <input type="password" id="wachtwoord" name="wachtwoord"><br>
