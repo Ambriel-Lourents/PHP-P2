4131562368097f1742e599cc5c8792d76e8d6fd4
Ambriel-Lourents <112871854+Ambriel-Lourents@users.noreply.github.com>
2023-01-16 15:05:14 +0100

    opdrachten

---
+++ opdracht_3.2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $number = 5;
+    $num1 = 6;
+    $num2 = 10;
+    $num3 = $num1 + $num2 + $number;
+    echo "<p>  $number + $num1 + $num2 = $num3  </p>"
+    ?>
+
+</body>
+</html>
