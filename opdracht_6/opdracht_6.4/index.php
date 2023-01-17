8dee67e8b06a47ec2a2ef46d4e7a2ffa99cc4cc3
Ambriel-Lourents <112871854+Ambriel-Lourents@users.noreply.github.com>
2023-01-17 18:25:44 +0100

    opdracht_6.4 WERKT!!!!!!!!

--- opdracht_6/opdracht_6.4/index.php
+++ opdracht_6/opdracht_6.4/index.php
@@ -10,24 +10,29 @@
 
 <body>
 
-    <form action="" method="$_POST">
+    <form action="" method="post">
 
         <input type="number" name="getal" value="prijs">Prijs <br>
-        <input type="number" name="getal" value="korting">korting % <br>
-        <input type="Submit" name="submit" value="submit"> <br>
+        <input type="number" name="korting" value="korting">Korting % <br>
+        <input type="submit" name="submit" value="Submit"> <br>
     </form>
 
     <?php
 
 
-if (isset($_POST['submit'])) {
+    if (isset($_POST['submit'])) {
+        if (isset($_POST['getal'])) {
+            $getal = $_POST['getal'];
+            if (isset($_POST['korting'])) {
+            $korting = $_POST['korting'];
+$answer = ($_POST['getal']) / 100 * ($_POST['korting']) - $getal;
 
 
-
-    
+            echo " Bedrag inclusief $korting% korting: $answer ";
+    }
 }
-
-?>
+    }
+    ?>
 
 
 
